95bc8f87e5431565cb50bc76172d20b3d80714a8
mohammad-alshish <eng.moahammed.majed@gamil.com>
2022-06-06 16:03:56 +0300

    ok

---
+++ madlib_cli/madlib_cli.py
@@ -0,0 +1,10 @@
+welcome_message = """
+  *************************************************
+             Welcome to MadLib's lab!
+  *************************************************
+  """
+def welcome():
+  """
+  Prints welcome message to user upon application start!
+  """
+print(welcome_message)
